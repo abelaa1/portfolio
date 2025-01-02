f71573babbb42bda2ea7d7e50ae0aa962686c45a
Abel <aaguilar61@gatech.edu>
2025-01-02 00:12:26 -0800

    readme

---
+++ .svelte-kit/generated/client/nodes/4.js
@@ -0,0 +1,3 @@
+import * as universal from "../../../../src/routes/education/[slug]/+page.ts";
+export { universal };
+export { default as component } from "../../../../src/routes/education/[slug]/+page.svelte";
